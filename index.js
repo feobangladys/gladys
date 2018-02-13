04502dac11c54b9f039aed83b6042b96064933ab
feobangladys <feoban@gmail.com>
2018-02-13 23:01:09 +0100

    test

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var ip = require('./lib/ip.js');
+var getip = require('./lib/getip.js');
+var command = require('./lib/getip.command.js');
+var install = require('./lib/install.js');
+var uninstall = require('./lib/uninstall.js');
 
 module.exports = function(sails) {
-    return {
-        ip: ip
-    };
+	return {
+		getip: getip,
+		command: command,
+		install: install,
+		uninstall: uninstall
+	};
 };
